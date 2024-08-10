de8fafdfffbd71e02cf286ab9831c671ef1f31f0
Josh <ijoshwang@gmail.com>
2024-08-10 12:49:24 +0800

    style: change mobile icon

--- src/components/Header/MobileNav.tsx
+++ src/components/Header/MobileNav.tsx
@@ -1,25 +1,23 @@
-"use client"
+'use client';
 
-import React from "react"
+import React from 'react';
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
-  SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet"
-import { Button } from "@/components/ui/button"
-import { Egg, Menu } from "lucide-react"
-import Link, { LinkProps } from "next/link"
-import { useRouter } from "next/navigation"
-import { cn } from "@/lib/utils"
-import { siteConfig } from "@/config/site"
-import { navConfig } from "@/config"
-import { ScrollArea } from "@/components/ui/scroll-area"
+} from '@/components/ui/sheet';
+import { Button } from '@/components/ui/button';
+import { Presentation, Menu } from 'lucide-react';
+import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { siteConfig } from '@/config/site';
+import { navConfig } from '@/config';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const MobileNav = () => {
-  const [open, setOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false);
 
   return (
     <Sheet open={open} onOpenChange={setOpen}>
@@ -34,18 +32,15 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent side="right" className="pr-0">
         <SheetHeader>
-          {/* <SheetTitle> */}
           <MobileLink
             href="/"
             className="flex items-center space-x-1"
             onOpenChange={setOpen}
           >
-            <Egg className="w-5 h-5" />
+            <Presentation className="w-5 h-5" />
             <span className="font-bold">{siteConfig.name}</span>
           </MobileLink>
-          {/* </SheetTitle> */}
         </SheetHeader>
-        {/* <SheetDescription> */}
         <ScrollArea className="h-[calc(100vh-8rem)] pb-10 pl-6 mt-4">
           <div className="flex flex-col space-y-3 text-left">
             {navConfig?.map(
@@ -62,16 +57,15 @@ const MobileNav = () => {
             )}
           </div>
         </ScrollArea>
-        {/* </SheetDescription> */}
       </SheetContent>
     </Sheet>
-  )
-}
+  );
+};
 
 interface MobileLinkProps extends LinkProps {
-  onOpenChange?: (open: boolean) => void
-  children: React.ReactNode
-  className?: string
+  onOpenChange?: (open: boolean) => void;
+  children: React.ReactNode;
+  className?: string;
 }
 
 function MobileLink({
@@ -81,20 +75,20 @@ function MobileLink({
   children,
   ...props
 }: MobileLinkProps) {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <Link
       href={href}
       onClick={() => {
-        router.push(href.toString())
-        onOpenChange?.(false)
+        router.push(href.toString());
+        onOpenChange?.(false);
       }}
       className={cn(className)}
       {...props}
     >
       {children}
     </Link>
-  )
+  );
 }
 
-export default MobileNav
+export default MobileNav;
