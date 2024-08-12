1a3c7452ce9764045bcebcb8b0c25320975cb03e
Josh <ijoshwang@gmail.com>
2024-08-12 14:11:40 +0800

    fix: version errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import TableList from '@/components/TableList'
 export default async function Home() {
   return (
     <main className="flex flex-col items-center justify-between gap-5 py-10">
-      <h2 className="text-xl font-medium">Hi, I'm Josh 👋</h2>
+      <h2 className="text-xl font-medium">Josh's Next Experiment 👋</h2>
       <div className="w-full">
         <TableList />
       </div>
